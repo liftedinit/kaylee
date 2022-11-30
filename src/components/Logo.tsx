3377147b4a8be8949b3d24cbf809114c04bc991b
Stanley Jones <stanley@sunshocked.com>
2022-11-30 14:47:10 -0800

    feat: Show network status when connected

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
+import { Image } from "@liftedinit/ui";
 import logo from "../logo.png";
-import "./Logo.css";
 
 function Logo() {
-  return <img className="Logo" src={logo} alt="Kaylee by Lifted Labs" />;
+  return <Image src={logo} my={6} h={50} alt="Kaylee by Lifted Labs" />;
 }
 export default Logo;
